db64c0569bc14f8a843eb92c0fcac23324ffb151
yamashita <fko2347040@stu.o-hara.ac.jp>
2024-09-22 17:35:49 +0900

    signup、signin、signoutの作成

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('home', views.home, name='home'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,52 @@
+from unittest import loader
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, logout
+from app.models import CustomUser
 
-# Create your views here.
+def index(request):
+    context = {'user': request.user}
+    return render(request, 'index.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        new_user = CustomUser(first_name=first_name, last_name=last_name, username=username, email=email)
+        new_user.set_password(password)  # パスワードのハッシュ化
+        new_user.save()
+        
+        # signup_success.htmlでアラートを表示し、リダイレクトさせる
+        return render(request, 'signup_success.html', {'message': 'ユーザーの作成に成功しました'})
+    else:
+        return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user = CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return render(request, 'signin.html', {'error_message': 'ユーザーが存在しません。'})
+
+        if user.password == password:
+            login(request, user)
+            return HttpResponseRedirect('/home')
+        else:
+            return render(request, 'signin.html', {'error_message': 'パスワードが正しくありません。'})
+    else:
+        return render(request, 'signin.html')
+
+def signout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def home(request):
+    template = loader.get_template("app/home.html")
+    return HttpResponse(template.render({}, request))
