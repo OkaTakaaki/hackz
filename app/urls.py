eee1d2da440633c2f0a1c5eaeabef1c9cfea1c1e
Takaaki Oka <fko2347014@stu.o-hara.ac.jp>
2024-09-24 14:13:21 +0900

    calendar

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,4 @@ app_name = 'app'
 
 urlpatterns = [
     path('mycalendar/', views.MyCalendar.as_view(), name='mycalendar'),
-    path('mycalendar/<int:year>/<int:month>/<int:day>/', views.MyCalendar.as_view(), name='mycalendar_with_date'),
 ]
